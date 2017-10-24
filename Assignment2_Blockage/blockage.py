a83472e5c4be75a1dc4a5f255604f06a98d1d12a
arime <antoine.rime@student.uclouvain.be>
2017-10-25 01:12:12 +0200

    Shitty shitty code that get shits done

--- Assignment2_Blockage/blockage.py
+++ Assignment2_Blockage/blockage.py
@@ -9,12 +9,14 @@ from search import *
 #################
 class Blockage(Problem):
 
-    def gravity(self, state, blocks):
-        new_grid = list(state.grid[:])
+    def gravity(self, state, new_grid):
         transposed_grid = list(map(list, zip(*new_grid)))
+        blocks = list()
+        for x,row in enumerate(new_grid):
+            for y,item in enumerate(row):
+                if item.islower():
+                    blocks.append((x,y,item))
         for pos_x,pos_y,block in blocks:
-            row = list(new_grid[pos_x])
-            new_grid[pos_x] = tuple(row)
             # Transpose matrix to apply gravity
             gravity_col = transposed_grid[pos_y][:]
             for j in range(pos_x, state.nbr - 1):
@@ -57,13 +59,14 @@ class Blockage(Problem):
                 row = list(new_grid[pos_x])
                 row[i] = block
                 row[pos_y] = ' '
+                new_grid[pos_x] = tuple(row)
 
                 # Add a comment
                 cost = pos_y - i
                 comment = ''.join(['(', str(pos_x), ',', str(pos_y), ') ', 'move to ', 'left' if cost > 0 else 'right'])
 
-                gravity_applied_grid = self.gravity(state, blocks)
-                # print(State(tuple(gravity_applied_grid), comment=comment))
+                gravity_applied_grid = self.gravity(state, new_grid)
+                #print(State(tuple(gravity_applied_grid), comment=comment))
                 succ.append(('move', State(tuple(gravity_applied_grid), comment=comment)))
 
         for s in succ:
